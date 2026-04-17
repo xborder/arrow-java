b91353908971fb62baccec97e85e623133d683aa
Hélder Gregório <helder.gregorio@dremio.com>
2026-04-17 11:02:14 +0100

    Revert "GH-62: relax JDBC cancellation test assertion"
    This reverts commit b57550fa15c95a4011686f5d1addbba2186611d9.

--- flight/flight-sql-jdbc-core/src/test/java/org/apache/arrow/driver/jdbc/ResultSetTest.java
+++ flight/flight-sql-jdbc-core/src/test/java/org/apache/arrow/driver/jdbc/ResultSetTest.java
@@ -302,14 +302,14 @@ public class ResultSetTest {
   @Test
   public void testShouldInterruptFlightStreamsIfQueryIsCancelledMidQuerying()
       throws SQLException, InterruptedException {
-    final String query = CoreMockedSqlProducers.LEGACY_REGULAR_SQL_CMD;
     try (final Statement statement = connection.createStatement()) {
       final CountDownLatch latch = new CountDownLatch(1);
       final Set<Exception> exceptions = synchronizedSet(new HashSet<>(1));
       final Thread thread =
           new Thread(
               () -> {
-                try (final ResultSet resultSet = statement.executeQuery(query)) {
+                try (final ResultSet resultSet =
+                    statement.executeQuery(CoreMockedSqlProducers.LEGACY_REGULAR_SQL_CMD)) {
                   final int cachedColumnCount = resultSet.getMetaData().getColumnCount();
                   Thread.sleep(300);
                   while (resultSet.next()) {
@@ -332,14 +332,7 @@ public class ResultSetTest {
               .reduce(StringBuilder::append)
               .orElseThrow(IllegalArgumentException::new)
               .toString(),
-          anyOf(
-              is("Statement canceled"),
-              allOf(
-                  containsString(format("Error while executing SQL \"%s\"", query)),
-                  anyOf(
-                      containsString("Query canceled"),
-                      containsString("CANCELLED"),
-                      containsString("Cancelling")))));
+          is("Statement canceled"));
     }
   }
 
